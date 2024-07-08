8f7cadafcbb304a411ff3e47e3250481d355744d
tulsimundada <tulsimundada@gmail.com>
2024-07-08 21:28:17 +0530

    Jenkins test for java file

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+   public static void main(String[] args){
+       System.out.println("Hello World!");
+<<<<<<< HEAD
+       System.out.println("HI")
+=======
+       System.out.println("Hello!");
+>>>>>>> 7e03a858cb94d93b2fa9a6c120df3a17f4a6aa96
+    }
+}
